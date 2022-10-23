72325545ae90d2d5f2651f368a93698c4811c711
SomValentino <49089500+SomValentino@users.noreply.github.com>
2022-10-23 07:53:45 +0200

    added service installer

---
+++ Datacom.TaxCalculator.Infrastructure/ServiceInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Datacom.TaxCalculator.Domain.Interfaces;
+using Datacom.TaxCalculator.Infrastructure.Data;
+using Datacom.TaxCalculator.Infrastructure.Validation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datacom.TaxCalculator.Infrastructure
+{
+    public static class ServiceInstaller
+    {
+        public static void AddTaxCalculatorInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IDataContext, CsvDataContext>();
+            services.AddScoped<IDataValidator, CsvDataEntryValidator>();
+        }
+    }
+}
---
+++ Datacom.TaxCalculator.Logic/ServiceInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Datacom.TaxCalculator.Domain.Entities;
+using Datacom.TaxCalculator.Logic.Contracts;
+using Datacom.TaxCalculator.Logic.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datacom.TaxCalculator.Logic
+{
+    public static class ServiceInstaller
+    {
+        public static void AddTaxCalculatorLogicInstaller(this IServiceCollection services)
+        {
+            services.AddScoped<ITaxCalculator, Features.TaxCalculator>();
+            services.AddScoped<ITaxManager, TaxManager>();
+        }
+    }
+}
